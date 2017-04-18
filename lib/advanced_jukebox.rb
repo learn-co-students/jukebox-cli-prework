0d879dd403430296e2549237fb148636ed91f8a8
doulighan <doulighan@gmail.com>
2017-04-17 23:02:45 -0400

    done

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -2,19 +2,22 @@
 #make sure to edit the value of each key to replace < path to this directory >
 #with the correct path to this directory on your computer
 
-# my_songs = {
-# "Go Go GO" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/01.mp3',
-# "LiberTeens" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/02.mp3',
-# "Hamburg" =>  '< path to this directory >/jukebox-cli/audio/Emerald-Park/03.mp3',
-# "Guiding Light" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/04.mp3',
-# "Wolf" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/05.mp3',
-# "Blue" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/06.mp3',
-# "Graduation Failed" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/07.mp3'
-# }
+my_songs = {
+"Go Go GO" => 'Users/devinoulighan/Development/labs/jukebox-cli-prework/jukebox-cli/audio/Emerald-Park/01.mp3',
+"LiberTeens" => ' Users/devinoulighan/Development/labs/jukebox-cli-prework/jukebox-cli/audio/Emerald-Park/02.mp3',
+"Hamburg" =>  ' Users/devinoulighan/Development/labs/jukebox-cli-prework/jukebox-cli/audio/Emerald-Park/03.mp3',
+"Guiding Light" => ' Users/devinoulighan/Development/labs/jukebox-cli-prework/jukebox-cli/audio/Emerald-Park/04.mp3',
+"Wolf" => ' Users/devinoulighan/Development/labs/jukebox-cli-prework/jukebox-cli/audio/Emerald-Park/05.mp3',
+"Blue" => ' Users/devinoulighan/Development/labs/jukebox-cli-prework/jukebox-cli/audio/Emerald-Park/06.mp3',
+"Graduation Failed" => ' Users/devinoulighan/Development/labs/jukebox-cli-prework/jukebox-cli/audio/Emerald-Park/07.mp3'
+}
 
 def help
-  #this method should be the same as in jukebox.rb
-
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
 end
 
 
