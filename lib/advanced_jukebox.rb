b31e6441a42f420df38127074cabbd01511eebda
Ryan Mahaffey <ryan.a.mahaffey@gmail.com>
2017-04-18 02:07:45 +0000

    Done.

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -1,27 +1,27 @@
-#Here is the song hash you will be working with. Each key is a song name and each value is the location of it's mp3 file.
-#make sure to edit the value of each key to replace < path to this directory >
-#with the correct path to this directory on your computer
+# something is wrong. Either do not have the correct syntax for calling a file or my file paths are incorrect
 
-# my_songs = {
-# "Go Go GO" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/01.mp3',
-# "LiberTeens" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/02.mp3',
-# "Hamburg" =>  '< path to this directory >/jukebox-cli/audio/Emerald-Park/03.mp3',
-# "Guiding Light" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/04.mp3',
-# "Wolf" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/05.mp3',
-# "Blue" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/06.mp3',
-# "Graduation Failed" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/07.mp3'
-# }
+my_songs = {
+   "Go Go GO" => '/Users/Ryan/.atom/.learn-ide/home/ryanamahaffey-60602/code/labs/jukebox-cli-prework/audio/Emerald-Park/01.mp3',
+   "LiberTeens" => '/Users/Ryan/.atom/.learn-ide/home/ryanamahaffey-60602/code/labs/jukebox-cli-prework/audio/Emerald-Park/02.mp3',
+   "Hamburg" =>  '/Users/Ryan/.atom/.learn-ide/home/ryanamahaffey-60602/code/labs/jukebox-cli-prework/audio/Emerald-Park/03.mp3',
+   "Guiding Light" => '/Users/Ryan/.atom/.learn-ide/home/ryanamahaffey-60602/code/labs/jukebox-cli-prework/audio/Emerald-Park/04.mp3',
+   "Wolf" => '/Users/Ryan/.atom/.learn-ide/home/ryanamahaffey-60602/code/labs/jukebox-cli-prework/audio/Emerald-Park/05.mp3',
+   "Blue" => '/Users/Ryan/.atom/.learn-ide/home/ryanamahaffey-60602/code/labs/jukebox-cli-prework/audio/Emerald-Park/06.mp3',
+   "Graduation Failed" => '/Users/Ryan/.atom/.learn-ide/home/ryanamahaffey-60602/code/labs/jukebox-cli-prework/audio/Emerald-Park/07.mp3'
+ }
 
 def help
-  #this method should be the same as in jukebox.rb
-
+  puts "I accept the following commands:
+  - help : display this help message
+  - list : displays a list of songs you can play
+  - play : lets you choose a song to play
+  - exit : exits this program"
 end
 
 
 
 def list(my_songs)
-  #this method is different! Collect the keys of the my_songs hash and 
-  #list the songs by name
+  my_songs.keys.each_with_index {|song, idx| puts "#{idx+1}. #{song}"}
 end
 
 
@@ -33,13 +33,36 @@ def play(my_songs)
   #if it isn't, tell them their choice is invalid
   #if it is, play the song using the system 'open <file path>' syntax
   #get the file path of the song by looking it up in the my_songs hash
-  
+  puts "Enter a song name"
+  song = gets.chomp
+  if my_songs.keys.include?(song)
+    puts "Playing #{song}"
+    system open my_songs[song]
+  else
+    puts "Invalid input, please try again"
+  end
 end
 
 def exit_jukebox
-  #this method is the same as in jukebox.rb
+  puts "Goodbye"
 end
 
 def run(my_songs)
-  #this method is the same as in jukebox.rb
+  help
+  loop do
+    puts "\nPlease enter a command:\n"
+    response = gets.chomp
+    case response
+      when "exit"
+        return exit_jukebox
+      when "help"
+        help
+      when "list"
+        list(my_songs)
+      when "play"
+        play(my_songs)
+      else
+        puts "Invalid command, please try again."
+    end
+  end
 end
--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,3 +10,74 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+=begin
+def say_hello(name)
+  "Hi #{name}!"
+end
+
+puts "Enter your name:"
+users_name = gets.chomp
+
+puts say_hello(users_name)
+=end
+
+def help
+  puts "I accept the following commands:
+  - help : display this help message
+  - list : displays a list of songs you can play
+  - play : lets you choose a song to play
+  - exit : exits this program"
+end
+
+def list(songs_array)
+  songs_array.each_with_index {|song, idx| puts "#{idx+1}. #{song}"}
+end
+
+def play(songs_array)
+  puts "Please enter a song name or number:"
+  song = gets.chomp
+  song_found = false
+
+  if songs_array.include?(song)
+    song_found = true
+    puts "Playing #{song}"
+  else
+    song = song.to_i
+  end
+
+  if song_found == false
+    if song.is_a?(Fixnum) && song <= songs_array.size + 1 && song >= 1
+      song_found = true
+      puts "Playing #{songs_array[song - 1]}"
+    end
+  end
+
+  if song_found == false
+    puts "Invalid input, please try again"
+  end
+
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run(songs_array)
+  help
+  loop do
+    puts "\nPlease enter a command:\n"
+    response = gets.chomp
+    case response
+      when "exit"
+        return exit_jukebox
+      when "help"
+        help
+      when "list"
+        list(songs_array)
+      when "play"
+        play(songs_array)
+      else
+        puts "Invalid command, please try again."
+    end
+  end
+end
