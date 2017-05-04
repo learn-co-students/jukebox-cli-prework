825a4510c0e424e3a1766f0dcba400282ac9b8c0
Charles Wooley <cwooley89@gmail.com>
2017-05-04 17:59:41 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,3 +1,62 @@
+
+def run(songs)
+
+  help
+  input = ""
+  while input != "exit" do
+    puts "Please enter a command:"
+
+    input = gets.chomp
+    input = input.downcase
+
+    case input
+    when "help"
+      help
+    when "list"
+      list(songs)
+    when "play"
+      play(songs)
+    when "exit"
+      exit_jukebox
+    else
+      puts "Invalid command enter help for instructions."
+    end
+  end
+end
+
+def help
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  input = gets.chomp
+  if input.to_i > 0 && input.to_i < songs.size
+    input = input.to_i - 1
+    puts "Playing #{songs[input]}"
+  elsif songs.include?(input)
+    puts "Playing #{input}"
+  else
+    puts "Invalid input, please try again"
+  end
+end
+
+def list(songs)
+  songs.each_with_index do |song, index|
+    track_num = index + 1
+    puts "#{track_num}. #{song}"
+  end
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+
 songs = [
   "Phoenix - 1901",
   "Tokyo Police Club - Wait Up",
@@ -9,4 +68,3 @@ songs = [
   "Harry Chapin - Cats in the Cradle",
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
-
