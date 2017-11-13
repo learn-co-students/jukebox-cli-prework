932ae8ef31908b12bbaa5e1507474c29b0ff0e54
Eric Kollegger <eric.kollegger@gmail.com>
2017-11-13 18:01:00 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -28,15 +28,33 @@ end
 
 def play(songs)
   puts "Please enter a song name or number:"
-  user_input = gets.chomp
+  user_input = gets.downcase.chomp
   songs.each_with_index do |song, index|
-    index + 1
-    if user_input == song
-      puts "Playing #{song.to_s}"
-    elsif user_input == index
-      puts "Playing #{song.to_s}"
+    if user_input == song.downcase
+      puts "Playing #{song}"
+    elsif user_input.to_i == index + 1
+      puts "Playing #{song}"
     else
       puts "Invalid input, please try again"
     end
+  end
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run(songs)
+  help
+  puts "Please enter a command:"
+  response = gets.downcase.chomp
+  if response == 'list'
+    list(songs)
+  elsif response == 'play'
+    play(songs)
+  elsif response == 'help'
+    help
+  elsif response == 'exit'
+    exit_jukebox
   end
 end
