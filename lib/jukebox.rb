e884cbdd4cd893cc740519220202c49662f7274a
github username <github email address>
2018-05-01 17:39:24 +0200

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -25,11 +25,58 @@ end
 def list(songs_array)
   new_songs_array = []
   songs_array.each_with_index do |song, i|
-    binding.pry
-    new_songs_array << "#{i + 1}"
-
-
-
+    puts "#{i + 1}. #{song}"
   end
+end
+
+
+def play(songs_array)
+  puts "Please enter a song name or number:"
+  response = gets.chomp
+  songs_array.each_with_index do |song, i|
+    if response == song || response.to_i == i + 1
+      puts "Playing #{song}"
+      return nil
+    end
+  end
+  puts "Invalid input, please try again"
+end
+
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+
+def run(songs_array)
+  help
+  puts "Please enter a command:"
+  response = gets.chomp
+
+  loop do
+    if response == "exit"
+      exit_jukebox
+      break
+    end
+
+    if response == "help"
+      help
+      puts "Please enter a command:"
+      response = gets.chomp
+    end
+
+    if response == "list"
+      list(songs_array)
+      puts "Please enter a command:"
+      response = gets.chomp
+    end
+
+    if response == "play"
+      play(songs_array)
+      puts "Please enter a command:"
+      response = gets.chomp
+    end
+  end
+
 
 end
