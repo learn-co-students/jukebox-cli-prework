8706ccaac94264f1c13e42aa43ed5c45f4c34b92
Ann Kim <annkim822@gmail.com>
2018-06-24 23:56:09 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,3 +10,46 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+def help
+  puts "(?=.*help)(?=.*list)(?=.*play)(?=.*exit)."
+end
+
+def list(songs)
+  songs.each.with_index { |song, i|
+    puts "#{i+1} #{song}"
+  }
+end
+
+def play(songs)
+  selection = gets.downcase.strip
+  song_output = "Invalid input, please try again"
+  songs.each.with_index { |song, i|
+    song_output = "#{i+1} #{song}" if(selection == song.downcase || selection == "#{i+1}")
+  }
+  puts song_output
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run(songs)
+  puts "Please enter a command:"
+  command = gets.downcase.strip
+
+  while command != "exit"
+    case command
+      when "help"
+        help
+      when "list"
+        list(songs)
+      when "play"
+        play(songs)
+      else
+        puts "Invalid command, please try again"
+    end
+    puts "Please enter a command:"
+    command = gets.downcase.strip
+  end
+  exit_jukebox
+end
