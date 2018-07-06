120ae5e75ea207482923505736b7474e168ed57b
github username <github email address>
2018-07-05 23:40:31 -0500

    jukebox commit

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -22,13 +22,43 @@ end
 
 def list(songs)
   i = 0
-  binding.pry
   until i == songs.length-1
     puts "#{i+1}. #{songs[i]}"
     i += 1
   end
 end
 
-def songs
-  
+def play(songs)  
+  puts "Please enter a song name or number:"
+  name_num = gets.chomp
+    if songs.include?(name_num)
+      puts "Playing: #{name_num}"
+    elsif name_num.to_i > 0 && name_num.to_i < 10
+      puts "Playing: #{songs[name_num.to_i-1]}"
+    else
+      puts "Invalid input, please try again"
+    end
 end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run(songs)
+  puts help
+  puts "Please enter a command:"
+  cmd = gets.chomp
+  if cmd == "help"
+    puts help
+  elsif cmd == "play"
+    puts play
+  elsif cmd == "list"
+    puts list
+  elsif cmd == "exit"
+    puts exit_jukebox
+    
+  end
+
+
+end
+
