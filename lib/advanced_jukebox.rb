a4523e8b239495f95fbaef7bbaca4072203a31d7
Andrew Och <andrewoch95@gmail.com>
2018-07-12 19:13:39 +0000

    Done.

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -2,19 +2,23 @@
 #make sure to edit the value of each key to replace < path to this directory >
 #with the correct path to this directory on your computer
 
-# my_songs = {
-# "Go Go GO" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/01.mp3',
-# "LiberTeens" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/02.mp3',
-# "Hamburg" =>  '< path to this directory >/jukebox-cli/audio/Emerald-Park/03.mp3',
-# "Guiding Light" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/04.mp3',
-# "Wolf" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/05.mp3',
-# "Blue" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/06.mp3',
-# "Graduation Failed" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/07.mp3'
-# }
+my_songs = {
+"Go Go GO" => '/home/aoch1019/jukebox-cli-prework/audio/Emerald-Park/01.mp3',
+"LiberTeens" => '/home/aoch1019/jukebox-cli-prework/audio/Emerald-Park/02.mp3',
+"Hamburg" =>  '/home/aoch1019/jukebox-cli-prework/audio/Emerald-Park/03.mp3',
+"Guiding Light" => '/home/aoch1019/jukebox-cli-prework/audio/Emerald-Park/04.mp3',
+"Wolf" => '/home/aoch1019/jukebox-cli-prework/audio/Emerald-Park/05.mp3',
+"Blue" => '/home/aoch1019/jukebox-cli-prework/audio/Emerald-Park/06.mp3',
+"Graduation Failed" => '/home/aoch1019/jukebox-cli-prework/audio/Emerald-Park/07.mp3'
+}
 
 def help
   #this method should be the same as in jukebox.rb
-
+  puts "I accept the following commands:
+  - help : displays this help message
+  - list : displays a list of songs you can play
+  - play : lets you choose a song to play
+  - exit : exits this program"
 end
 
 
@@ -22,6 +26,10 @@ end
 def list(my_songs)
   #this method is different! Collect the keys of the my_songs hash and 
   #list the songs by name
+  
+  my_songs.keys.each_with_index do |song, index|
+    puts "#{index + 1}. #{song}"
+  end
 end
 
 
@@ -37,9 +45,27 @@ def play(my_songs)
 end
 
 def exit_jukebox
-  #this method is the same as in jukebox.rb
+  puts "Goodbye"
 end
 
 def run(my_songs)
-  #this method is the same as in jukebox.rb
+  help
+  puts "Please enter a command:"
+  user_response = gets.chomp
+  
+  while user_response != "exit"
+    if user_response == "list"
+      list(my_songs)
+    elsif user_response == "play"
+      play(my_songs)
+    elsif user_response == "help"
+      help
+    elsif user_response == "exit"
+      exit_jukebox
+    else
+      "Not a valid command!"
+    end
+    user_response = gets.chomp
+  end
+  exit_jukebox
 end
--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 songs = [
   "Phoenix - 1901",
   "Tokyo Police Club - Wait Up",
@@ -10,3 +12,68 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+#def say_hello(name)
+#  "Hi #{name}!"
+#end
+ 
+#puts "Enter your name:"
+#users_name = gets.chomp
+ 
+#puts say_hello(users_name)
+
+def help
+  puts "I accept the following commands:
+- help : displays this help message
+- list : displays a list of songs you can play
+- play : lets you choose a song to play
+- exit : exits this program"
+end
+
+def list(songs)
+  songs.each_with_index do |song, index|
+    puts "#{index + 1}. #{song}"
+  end
+end
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  user_choice = gets.chomp
+  is_valid = "no"
+  
+  songs.each_with_index do |song, index|
+    if user_choice == (index + 1).to_s || user_choice == song
+      puts "Playing #{song}"
+      is_valid = "yes"
+    end
+  end
+  
+  if is_valid == "no"
+    puts "Invalid input, please try again"
+  end
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run(songs)
+  help
+  puts "Please enter a command:"
+  user_response = gets.chomp
+  
+  while user_response != "exit"
+    if user_response == "list"
+      list(songs)
+    elsif user_response == "play"
+      play(songs)
+    elsif user_response == "help"
+      help
+    elsif user_response == "exit"
+      exit_jukebox
+    else
+      "Not a valid command!"
+    end
+    user_response = gets.chomp
+  end
+  exit_jukebox
+end
