130ab7e3de45267c3122662cbaf80c26f620f3a4
Madeleine Tabing <madtab7@gmail.com>
2018-07-28 21:23:30 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,6 +10,18 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+songs = [
+  "Phoenix - 1901",
+  "Tokyo Police Club - Wait Up",
+  "Sufjan Stevens - Too Much",
+  "The Naked and the Famous - Young Blood",
+  "(Far From) Home - Tiga",
+  "The Cults - Abducted",
+  "Phoenix - Consolation Prizes",
+  "Harry Chapin - Cats in the Cradle",
+  "Amos Lee - Keep It Loose, Keep It Tight"
+]
+
 def help
   puts "I accept the following commands:
 - help : displays this help message
@@ -19,34 +31,35 @@ def help
 end
 
 def list(songs)
-  songs.each.with_index do |song,index|
-    puts "#{index+1}. #{song}"
+  songs.each_with_index do |song, i|
+    puts "#{i+1}. #{song}"
   end
 end
 
 def play(songs)
   puts "Please enter a song name or number:"
-  song=gets.chomp
-  if songs.include?(song)
-    puts "Playing #{song}."
-  elsif song.to_i <= songs.length && song.to_i > 0
-    song = songs[song.to_i-1]
-    puts "Playing #{song}."
+  input = gets.chomp
+  if input.to_i < songs.length && input.to_i > 0
+    puts "Playing #{songs[input.to_i - 1]}"
   else
-    puts "Invalid input, please try again."
+    if songs.include?(input)
+      puts "Playing #{input}"
+    else
+      puts "Invalid input, please try again"
+    end
   end
 end
 
 def exit_jukebox
-  puts "Goodbye."
+  puts "Goodbye"
 end
 
 def run(songs)
   help
   puts "Please enter a command:"
-  response = gets.chomp
-  until response == "exit" do
-    case response
+  input = gets.chomp
+  until input == "exit" do
+    case input
     when "list"
       list(songs)
     when "help"
@@ -54,8 +67,11 @@ def run(songs)
     when "play"
       play(songs)
     else
-      puts "Invalid input. Please try again."
+      puts "Invalid input. Please try again"
     end
+
+    puts "Please enter a command:"
+    input = gets.chomp
   end
   exit_jukebox
 end
