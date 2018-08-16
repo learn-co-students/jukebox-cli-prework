37a398001007da5d7aa909af0fdce35aac44eac9
Jacob Berman <jacobberman1995@gmail.com>
2018-08-16 23:53:07 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,3 +1,4 @@
+
 songs = [
   "Phoenix - 1901",
   "Tokyo Police Club - Wait Up",
@@ -25,4 +26,36 @@ def list(songs)
 end 
 
 def play(songs)
+  puts("Please enter a song name or number:")
+  song_choice = gets.chomp
+  if songs.include?(song_choice)
+    puts "Playing #{song_choice}"
+  elsif ["1","2", "3", "4", "5","6", "8", "9"].include?(song_choice)
+    puts "Playing #{songs[song_choice.to_i-1]}"
+  else 
+    puts("Invalid input, please try again")
+  end 
+end 
+
+def exit_jukebox
+  puts "Goodbye"
+end 
+
+def run(songs)
+  help
+  puts("Please enter a command:")
+  user_input = nil
+  unless user_input == "exit"
+    puts("Please enter a command:")
+    user_input = gets.chomp
+    if user_input == "list"
+      list
+    elsif user_input == "play"
+      play
+    elsif user_input == "help"
+      help  
+    elsif user_input == "exit"
+      exit_jukebox
+    end 
+  end 
 end 
