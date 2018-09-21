e42d788984351b89dbb4ab50af0480654912e64c
Dario Spina <dario_spina01@live.it>
2018-09-21 08:37:24 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -9,4 +9,24 @@ songs = [
   "Harry Chapin - Cats in the Cradle",
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
+def help
+puts "I accept the following commands:
+- help : displays this help message
+- list : displays a list of songs you can play
+- play : lets you choose a song to play
+- exit : exits this program"
+end
+
+def list(songs)
+songs.each_with_index {|song,index|
+  puts"#{index+1}. #{song}"}
+end
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  user_choice=gets.chomp
+  if (songs.include?(user_choice) || user_choice<songs.length)
+    puts
+end
+
 
