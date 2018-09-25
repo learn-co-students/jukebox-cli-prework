08e600f8cb275702d6ecfc81f0ba399ba38c84e4
Joseph Arias <arias.joseph.a@gmail.com>
2018-09-25 01:34:12 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,3 +1,5 @@
+
+
 songs = [
   "Phoenix - 1901",
   "Tokyo Police Club - Wait Up",
@@ -10,3 +12,34 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+def help
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end 
+
+def list(songs)
+  song_each_with_index do |song, index|
+    puts "#{index + 1}. #{song}"
+  end
+end 
+
+def play(songs)
+ 
+  puts "Please enter a song name or number:"
+  user_response = gets.chomp
+end 
+
+def exit_jukebox 
+  puts "Goodbye"
+end 
+
+
+
+  
+  
+  
+  
+  
