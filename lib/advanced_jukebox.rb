4a71b39a9071f9ef1d0299dea0dd801c6605ea48
ruthmesfun <ruth.mesfun@gmail.com>
2018-10-09 21:10:04 -0400

    Done.

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -14,14 +14,21 @@
 
 def help
   #this method should be the same as in jukebox.rb
+  puts "I accept the following commands:\n
+  - help : displays this help message\n
+  - list : displays a list of songs you can play\n
+  - play : lets you choose a song to play\n
+  - exit : exits this program"
+
 
 end
 
 
 
 def list(my_songs)
-  #this method is different! Collect the keys of the my_songs hash and 
-  #list the songs by name
+  my_songs.each_key do |title|
+    puts "#{title}"
+  end
 end
 
 
@@ -33,13 +40,35 @@ def play(my_songs)
   #if it isn't, tell them their choice is invalid
   #if it is, play the song using the system 'open <file path>' syntax
   #get the file path of the song by looking it up in the my_songs hash
-  
+
+  puts "Please enter a song name or number:"
+  song_pick = gets.chomp
+  if my_songs.key?(song_pick)
+    open my_songs[song_pick]
+  else
+    puts "Invalid input, please try again"
+  end
+
 end
 
 def exit_jukebox
-  #this method is the same as in jukebox.rb
+  puts "Goodbye"
 end
 
 def run(my_songs)
   #this method is the same as in jukebox.rb
+  help
+  puts "Please enter a command:"
+  command = gets.chomp
+
+  case command
+  when "help"
+    help
+  when "list"
+    list(my_songs)
+  when "play"
+    play(my_songs)
+  when "exit"
+    exit_jukebox
+  end
 end
