1863952224680238766f4a7febd21799fd9bf2e9
Joshua Cocciardi <joshua.cocciardi@gmail.com>
2018-11-18 05:20:22 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -11,47 +11,47 @@ songs = [
 ]
 
 def help 
-  help = "I accept the following commands:
+  puts "I accept the following commands:
   - help : displays this help message
   - list : displays a list of songs you can play
   - play : lets you choose a song to play
   - exit : exits this program"
 end
 
-# def list(array)
-#   array.each_with_index do |value, index|
-#     puts "#{index+1}. #{value}"
-#   end
-# end
+def list(array)
+  array.each_with_index do |value, index|
+    puts "#{index+1}. #{value}"
+  end
+end
   
 def play(array)
   puts "Please enter a song name or number:"
-  binding.pry
   user_input = gets.chomp
   array.each_with_index do |value, index|
-    if user_input == value || user_input == "#{index+1}." 
+    if user_input == value || user_input == "#{index+1}" 
       puts "Playing <#{value}>"
     end
   end
   puts "Invalid input, please try again."
 end
 
-#def exit_jukebox
-#  puts "Goodbye."
-#end
+def exit_jukebox
+  puts "Goodbye."
+end
 
-#def run(array) 
-#  help 
-#  puts "Please enter a command:"
-#  user_input = gets.chomp
-#  until user_input == "exit"
-#    if user_input == "list"
-#      list
-#    elsif user_input == "help"
-#      help
-#    elsif user_input == "play"
-#      play
-#    end
-#    user_input = gets.chomp 
-#  end
-#end
+def run(array) 
+  help 
+  puts "Please enter a command:"
+  user_input = gets.chomp
+  until user_input == "exit"
+    if user_input == "list"
+      list
+    elsif user_input == "help"
+      help
+    elsif user_input == "play"
+      play
+    end
+    user_input = gets.chomp 
+  end
+end
+
