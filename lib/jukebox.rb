68d55df0f873864b055e69459bb5877253ec9d70
Joshua Cocciardi <joshua.cocciardi@gmail.com>
2018-11-18 18:46:02 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -43,18 +43,16 @@ def run(array)
   help 
   puts "Please enter a command:"
   user_input = gets.chomp
-  until user_input == "exit"
     if user_input == "list"
-      list
+      list(array)
     elsif user_input == "help"
       help
     elsif user_input == "play"
-      play
+      play(array)
     elsif user_input == "exit"
-      help
       exit_jukebox
     end
+      help
     user_input = gets.chomp 
-  end
-end
 
+end
