56838d6cdf8828da0d48c132e3909155d293c23f
Dania Herrera <danherrera0@gmail.com>
2018-12-01 02:56:01 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,3 +10,52 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+def help
+puts "I accept the following commands:
+- help : displays this help message
+- list : displays a list of songs you can play
+- play : lets you choose a song to play
+- exit : exits this program"
+end
+
+def list(songs)
+  puts "
+  1. Phoenix - 1901
+  2. Tokyo Police Club - Wait Up
+  3. Sufjan Stevens - Too Much
+  4. The Naked and the Famous - Young Blood
+  5. (Far From) Home - Tiga
+  6. The Cults - Abducted
+  7. Phoenix - Consolation Prizes
+  8. Harry Chapin - Cats in the Cradle
+  9. Amos Lee - Keep It Loose, Keep It Tight"
+end
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  song = gets.chomp
+  if songs.include(song)
+    puts "Playing #{song}"
+  else puts "Invalid input, please try again"
+  end
+
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run
+  help
+puts "Please enter a command:"
+input= gets.chomp
+if input == "play"
+  play
+if input == "list"
+  list
+if input == "help"
+  help
+if input == "exit"
+  exit_jukebox
+  break
+end
+  
