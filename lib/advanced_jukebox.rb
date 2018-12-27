9d8a557bf146c35c2b4dcfdb05e3a7930a9a0a72
mkay0961 <mkay0961@gmail.com>
2018-12-26 22:08:54 -0500

    Done.

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -2,7 +2,7 @@
 #make sure to edit the value of each key to replace < path to this directory >
 #with the correct path to this directory on your computer
 require 'pry'
-# my_songs = {
+# songs = {
 # "Go Go GO" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/01.mp3',
 # "LiberTeens" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/02.mp3',
 # "Hamburg" =>  '< path to this directory >/jukebox-cli/audio/Emerald-Park/03.mp3',
@@ -11,9 +11,7 @@ require 'pry'
 # "Blue" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/06.mp3',
 # "Graduation Failed" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/07.mp3'
 # }
-
 def help
-  #this method should be the same as in jukebox.rb
 
   puts "I accept the following commands:"
   puts "- help : displays this help message"
@@ -24,28 +22,25 @@ def help
 
 end
 
-
-
 def list(my_songs)
   #this method is different! Collect the keys of the my_songs hash and
   #list the songs by name
-  binding.pry
-
-
+  i=0
+  my_songs.each do |x, y|
+    puts "#{i}. #{x}"
+    i+=1
+  end
 end
 
-
 def play(my_songs)
-  #this method is slightly different!
-  #you should still ask the user for input and collect their song choice
-  #this time, only allow user's to input a song name
-  #check to see if the name they give is in fact a key of the my_songs hash
-  #if it isn't, tell them their choice is invalid
-  #if it is, play the song using the system 'open <file path>' syntax
-  #get the file path of the song by looking it up in the my_songs hash
-  puts "Please enter a song name or number:"
-  resp = gets.comp
-
+  puts "Please enter a song name:"
+  resp = gets.chomp
+  if my_songs.keys.include?(resp)
+    puts "Now playing #{resp}"
+    system 'open ' << my_songs[resp]
+  else
+    puts "Invalid input, please try again"
+  end
 end
 
 def exit_jukebox
@@ -55,4 +50,21 @@ end
 
 def run(my_songs)
   #this method is the same as in jukebox.rb
+  help
+  response = ""
+  while response
+      puts "Please enter a command:"
+      response = gets.downcase.strip
+      if response == "list"
+        list(my_songs)
+      elsif response == "play"
+        play(my_songs)
+      elsif response == "help"
+        help
+      elsif response == "exit"
+        exit_jukebox
+        break
+
+      end
+    end
 end
