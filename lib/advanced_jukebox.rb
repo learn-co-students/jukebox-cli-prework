2b8e4357f3d3c6e647d75c470406cdb203e0bd63
github username <github email address>
2019-01-12 13:29:42 -0500

    Completed Jukebox CLI Lab

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -3,7 +3,7 @@
 #with the correct path to this directory on your computer
 
 # my_songs = {
-# "Go Go GO" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/01.mp3',
+# "Go Go GO" => '< path to this directory >/audio/Emerald-Park/01.mp3',
 # "LiberTeens" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/02.mp3',
 # "Hamburg" =>  '< path to this directory >/jukebox-cli/audio/Emerald-Park/03.mp3',
 # "Guiding Light" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/04.mp3',
@@ -13,15 +13,23 @@
 # }
 
 def help
-  #this method should be the same as in jukebox.rb
-
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
 end
 
 
 
 def list(my_songs)
-  #this method is different! Collect the keys of the my_songs hash and 
+  #this method is different! Collect the keys of the my_songs hash and
   #list the songs by name
+  counter = 1
+    my_songs.each do |song, path|
+      puts "#{counter}. #{song}"
+      counter += 1
+    end
 end
 
 
@@ -33,13 +41,38 @@ def play(my_songs)
   #if it isn't, tell them their choice is invalid
   #if it is, play the song using the system 'open <file path>' syntax
   #get the file path of the song by looking it up in the my_songs hash
-  
+  puts "Please enter a song name:"
+  user_response = gets.chomp
+      if my_songs.include?(user_response)
+        puts "Playing #{user_response}"
+        system "open #{my_songs[user_response]}"
+      else
+        puts "Invalid input, please try again"
+      end
 end
 
 def exit_jukebox
-  #this method is the same as in jukebox.rb
+  puts "Goodbye"
 end
 
+
 def run(my_songs)
-  #this method is the same as in jukebox.rb
+help
+user_response = ""
+while user_response
+puts "Please enter a command:"
+user_response = gets.downcase.strip
+    if user_response ==  "list"
+      list(my_songs)
+    elsif user_response == "play"
+      play(my_songs)
+    elsif user_response == "help"
+      help
+    elsif user_response == "exit"
+      exit_jukebox
+      break
+    else
+      help
+    end
+  end
 end
