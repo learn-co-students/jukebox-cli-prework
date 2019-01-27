43111384dafc091de6594b87c1ed320f632235ac
Eric Hardiman <hardimaneric@gmail.com>
2019-01-27 00:29:14 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -17,3 +17,49 @@ def help
   puts "- play : lets you choose a song to play"
   puts "- exit : exits this program"
 end
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  going_to_play = gets.chomp
+  if (1..9).to_a.include?(going_to_play.to_i)
+    puts "Playing #{songs[going_to_play.to_i - 1]}"
+  elsif songs.include?(going_to_play)
+    puts "Playing #{going_to_play}"
+  else
+    puts "Invalid input, please try again"
+  end
+end
+
+def list(songs)
+  songs.each_with_index do |song, i|
+    puts "#{i+1}. #{song}"
+  end
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run(songs)
+  help
+
+  input = ""
+  while input
+    puts "Please enter a command:"
+    input = gets.downcase.chomp
+    case input
+    when 'list'
+      list(songs)
+    when 'play'
+      list(songs)
+      play(songs)
+    when 'help'
+      help
+    when 'exit'
+      exit_jukebox
+      break
+    else
+      help
+    end
+  end
+end
