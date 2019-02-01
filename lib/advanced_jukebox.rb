e8a81b1a0721c4148d0f3488711cdf570593dc3a
stephenyoon84 <stephenyoon84@gmail.com>
2019-02-01 12:21:13 -0500

    Jukebox-cli - Done

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -1,27 +1,34 @@
 #Here is the song hash you will be working with. Each key is a song name and each value is the location of it's mp3 file.
 #make sure to edit the value of each key to replace < path to this directory >
 #with the correct path to this directory on your computer
-
+require 'pry'
 # my_songs = {
-# "Go Go GO" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/01.mp3',
-# "LiberTeens" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/02.mp3',
-# "Hamburg" =>  '< path to this directory >/jukebox-cli/audio/Emerald-Park/03.mp3',
-# "Guiding Light" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/04.mp3',
-# "Wolf" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/05.mp3',
-# "Blue" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/06.mp3',
-# "Graduation Failed" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/07.mp3'
+# "Go Go GO" => 'Users/StephenYOON/Desktop/FlatironSchoolPrep/FirstLearnLab/10.Your-First-Application/jukebox-cli-prework/audio/Emerald-Park/01.mp3',
+# "LiberTeens" => 'Users/StephenYOON/Desktop/FlatironSchoolPrep/FirstLearnLab/10.Your-First-Application/jukebox-cli-prework/audio/Emerald-Park/02.mp3',
+# "Hamburg" =>  'Users/StephenYOON/Desktop/FlatironSchoolPrep/FirstLearnLab/10.Your-First-Application/jukebox-cli-prework/audio/Emerald-Park/03.mp3',
+# "Guiding Light" => 'Users/StephenYOON/Desktop/FlatironSchoolPrep/FirstLearnLab/10.Your-First-Application/jukebox-cli-prework/audio/Emerald-Park/04.mp3',
+# "Wolf" => 'Users/StephenYOON/Desktop/FlatironSchoolPrep/FirstLearnLab/10.Your-First-Application/jukebox-cli-prework/audio/Emerald-Park/05.mp3',
+# "Blue" => 'Users/StephenYOON/Desktop/FlatironSchoolPrep/FirstLearnLab/10.Your-First-Application/jukebox-cli-prework/audio/Emerald-Park/06.mp3',
+# "Graduation Failed" => 'Users/StephenYOON/Desktop/FlatironSchoolPrep/FirstLearnLab/10.Your-First-Application/jukebox-cli-prework/audio/Emerald-Park/07.mp3'
 # }
 
 def help
   #this method should be the same as in jukebox.rb
-
+  puts "I accept the following commands:\n
+  - help : displays this help message\n
+  - list : displays a list of songs you can play\n
+  - play : lets you choose a song to play\n
+  - exit : exits this program"
 end
 
 
 
 def list(my_songs)
-  #this method is different! Collect the keys of the my_songs hash and 
+  #this method is different! Collect the keys of the my_songs hash and
   #list the songs by name
+  my_songs.keys.each_with_index do |song, i|
+    puts "#{i + 1}. #{song}"
+  end
 end
 
 
@@ -33,13 +40,38 @@ def play(my_songs)
   #if it isn't, tell them their choice is invalid
   #if it is, play the song using the system 'open <file path>' syntax
   #get the file path of the song by looking it up in the my_songs hash
-  
+  puts "Please enter a song name:"
+  user_input = gets.chomp
+  if !my_songs.keys.include?(user_input)
+      puts "Invalid input, please try again"
+    else
+      puts "Now playing #{user_input}"
+      # binding.pry
+      system "open #{my_songs[user_input]}" #'open ' << my_songs[user_input]
+  end
 end
 
 def exit_jukebox
   #this method is the same as in jukebox.rb
+  puts "Goodbye"
 end
 
 def run(my_songs)
   #this method is the same as in jukebox.rb
+  user_command = ""
+  while user_command
+    puts "Please enter a command:"
+    user_command = gets.chomp.downcase
+    case user_command
+    when "list"
+      list(my_songs)
+    when "play"
+      play(my_songs)
+    when "exit"
+      exit_jukebox
+      break
+    else
+      help
+    end
+  end
 end
--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,3 +1,4 @@
+require 'pry'
 songs = [
   "Phoenix - 1901",
   "Tokyo Police Club - Wait Up",
@@ -10,3 +11,49 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+def help
+  puts "I accept the following commands:\n
+  - help : displays this help message\n
+  - list : displays a list of songs you can play\n
+  - play : lets you choose a song to play\n
+  - exit : exits this program"
+end
+
+def list(songs)
+  songs.each_with_index {|song, index| puts "#{index + 1}. #{song}"}
+end
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  user_input = gets.chomp
+  if (1..9).include?(user_input.to_i)
+    puts "Playing #{songs[user_input.to_i - 1]}"
+  elsif songs.include?(user_input)
+    puts "Playing #{user_input}"
+  else
+    puts "Invalid input, please try again"
+  end
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run(songs)
+  user_command = ""
+  while user_command
+    puts "Please enter a command:"
+    user_command = gets.chomp.downcase
+    case user_command
+    when "list"
+      list(songs)
+    when "play"
+      play(songs)
+    when "exit"
+      exit_jukebox
+      break
+    else
+      help
+    end
+  end
+end
