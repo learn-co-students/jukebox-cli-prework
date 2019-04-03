68473c845deab5bbf8f804c87930e6b03eb50d47
Noah Fairbairn <noahfairbairn@gmail.com>
2019-04-03 20:51:52 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,7 +1,6 @@
 require 'pry'
 
 def help
-  binding.pry
   puts "I accept the following command:"
   puts "- help : displays this help message"
   puts "- list : displays a list of songs you can play"
@@ -17,10 +16,33 @@ def play(songs)
   puts "Please enter a song name or number:"
   user_response = gets.chomp
   songs.each_with_index{|song, index|
-    if user_response == song || user_response == (index + 1)
+    real_index = "#{index+1}"
+    if user_response == song || user_response == real_index
       puts "Playing #{song}"
     else
       puts "Invalid input, please try again"
     end
   }
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run(songs)
+  help
+  puts "Please enter a command:"
+  user_input = gets.chomp.downcase
+    case user_input
+      when "list"
+        list(songs)
+      when "help"
+        help
+      when "play"
+        play(songs)
+      when "exit"
+        exit_jukebox
+      else
+        "Invalid Command"
+    end
 end
