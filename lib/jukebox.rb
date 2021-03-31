78c49e9cb60d3f7107e782058bf6719dfaececc4
Winston Tokuhisa <tokuhisawinston@outlook.com>
2021-03-31 16:30:58 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,3 +10,42 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+# def say_hello(name)
+  # "Hi #{name}!"
+# end
+ 
+# puts "Enter your name:"
+# users_name = gets.strip
+ 
+# puts say_hello(users_name)
+
+def help
+  puts 'I accept the following commands:'
+  puts '- help : displays this help message'
+  puts '- list : displays a list of songs you can play'
+  puts '- play : lets you choose a song to play'
+  puts '- exit : exits this program'
+end
+
+def list(songs)
+  songs.each_with_index do | song, i |
+    puts "#{i + 1}. #{song}"
+  end
+end
+
+# Find song by index
+# validate song name
+
+def play(songs)
+  # puts 'Please enter a song name or number:'
+  # input = gets.strip
+  
+  # # Check if input is an integer
+  # if input.to_i.class == Integer
+  #   # If yes, check if integer is in range
+  #   puts "Playing #{songs[input.to_i - 1]}"
+  # end
+  #   # If no, check if name is valid
+end
+
+play(songs)
